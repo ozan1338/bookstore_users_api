0ea6196165452455a4f5729077c8e79c2becec5d
ozan1338 <akhmadfauzan1338@gmail.com>
2022-10-07 11:11:30 +0700

    Create Domain for user, and handling error

--- controllers/user/users_controller.go
+++ controllers/user/users_controller.go
@@ -1,7 +1,12 @@
 package user
 
 import (
+	"fmt"
 	"net/http"
+	"users_api/domain/users"
+	"users_api/services"
+
+	resError "users_api/utils/errors"
 
 	"github.com/gin-gonic/gin"
 )
@@ -13,9 +18,27 @@ func GetUser(c *gin.Context) {
 }
 
 func CreateUser(c *gin.Context) {
-	c.JSON(http.StatusNotImplemented, gin.H{
-		"status": "Not Ready",
-	})
+	var user users.User
+
+	//This function is same as we Readall and unmarshall the request
+	if err := c.ShouldBindJSON(&user); err != nil {
+		//TODO: handle json error
+		var resErr resError.RestError
+		resErr = *resError.NewBadRequestError("invalid json body")
+		c.JSON(http.StatusBadRequest, resErr)
+		return
+	}
+
+	fmt.Println(user)
+
+	result, err := services.CreateUser(user)
+	if err != nil {
+		//TODO: handle error
+		c.JSON(err.Status, err)
+		return
+	}
+
+	c.JSON(http.StatusCreated, result)
 }
 
 func SearchUser(c *gin.Context) {
--- services/users_service.go
+++ services/users_service.go
@@ -1,1 +1,11 @@
 package services
+
+import (
+	"users_api/domain/users"
+	resError "users_api/utils/errors"
+)
+
+func CreateUser(user users.User) (*users.User, *resError.RestError) {
+
+	return &user,nil
+}
